419d7dbe791629c7218cb2635f2027f74dee6845
ClaytonBazar <claytonbazar04@gmail.com>
2023-07-13 15:24:44 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -12,6 +13,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
-endif
+#endif
 
